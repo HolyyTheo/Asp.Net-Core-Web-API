e09a4f164613f9ac3b26defe571d1feac8a14bfb
Oğuzhan Kandemir <oguzhankandemir11@gmail.com>
2024-11-28 11:58:48 +0300

    With Test asp and javascript project
    With Test asp and javascript project

---
+++ aspClientApp/Models/ProductDTO.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace aspClientApp.Models
+{
+    public class ProductDTO
+    {
+
+        [JsonPropertyName("productID")]
+        public int ProductID { get; set; }
+        [JsonPropertyName("productName")]
+
+        public string ProductName { get; set; } = null!;
+
+        [JsonPropertyName("price")]
+
+        public decimal Price { get; set; }
+    }
+}
